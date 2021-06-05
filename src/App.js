a226400c64ca07105fbac23bf60a1da273c33b6c
preethasri <84436303+preethasri@users.noreply.github.com>
2021-06-05 00:18:05 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+const filmDB = {
+  Comic: [
+    { name: "Thillu Mullu", rating: "4.5/5" },
+    { name: "Avvai Shanmughi", rating: "4/5" }
+  ],
+  Action: [
+    { name: "Singam", rating: "4.5/5" },
+    { name: "thuppaki", rating: "4/5" }
+  ],
+  Romance: [
+    { name: "Mouna Ragam", rating: "4/5" },
+    { name: "alai payuthey", rating: "3.5/5" }
+  ],
+  Biopic: [
+    { name: "M.S Dhoni", rating: "4.5/5" },
+    { name: "mahanati", rating: "4/5" }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("Biopic");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> 🎬 Good south indian movies</h1>
+      <p style={{ fontSize: "smaller" }}>
+        {""}
+        checkout my favourite movies. select a genre to get started{""}
+      </p>
+      <div>
+        {Object.keys(filmDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              padding: "0.5rem 1rem",
+              cursor: "pointer",
+              background: "#A78BFA",
+              borderRadius: "0.5rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlinestart: "0" }}>
+          {filmDB[selectedGenre].map((film) => (
+            <li
+              key={film.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {""}
+
+              <div style={{ fontsize: "larger" }}> {film.name}</div>
+              <div style={{ fontsize: "smaller" }}> {film.rating}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
