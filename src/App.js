ca286f58f5fabf4da92509fc49c14469e4111c9d
preethasri <84436303+preethasri@users.noreply.github.com>
2021-07-12 00:02:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ export default function App() {
               key={film.name}
               style={{
                 listStyle: "none",
-                padding: "5rem",
+                marginTop: "20px",
                 border: "3px solid black",
                 width: "50%",
                 margin: "auto",
