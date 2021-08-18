1310a5bbdc9af6f9feadd6bfeab5f943b80d91f3
preethasri <84436303+preethasri@users.noreply.github.com>
2021-08-18 14:01:03 +0530

    commit added description

--- src/App.js
+++ src/App.js
@@ -4,20 +4,84 @@ import { useState } from "react";
 
 const filmDB = {
   Comic: [
-    { name: "Thillu Mullu", rating: "4.5/5" },
-    { name: "Avvai Shanmughi", rating: "4/5" }
+    {
+      name: "Thillu Mullu",
+      rating: "4.5/5",
+      description:
+        "Description:a young man fools his boss and attempts to woo his boss' daughter."
+    },
+    {
+      name: "Avvai Shanmughi",
+      rating: "4/5",
+      description:
+        "Description:A man disguises himself as a nanny to be with his daughter after his divorce."
+    },
+    {
+      name: "panchaTanthiram",
+      rating: "4.2/5",
+      description:
+        "Description:5 friends who enjoy a lot even after marriage and that ends in a confusion how they are handling  is the end of the story"
+    }
   ],
   Action: [
-    { name: "Singam", rating: "4.5/5" },
-    { name: "thuppaki", rating: "4/5" }
+    {
+      name: "Singam",
+      rating: "4.5/5",
+      description:
+        "Description:Honest police officer Durai Singam locks horns with big-time extortionist Mayil Vaaganam."
+    },
+    {
+      name: "thuppaki",
+      rating: "4/5",
+      description:
+        "Description:An army captain is on a mission to track down and destroy a terrorist gang and deactivate the sleeper cells under its command."
+    },
+    {
+      name: "virumaandi",
+      rating: "4.1/5",
+      description:
+        "Description:A temperamental farmer gets embroiled in a bloody clan feud in a southern Tamil Nadu village."
+    }
   ],
   Romance: [
-    { name: "Mouna Ragam", rating: "4/5" },
-    { name: "alai payuthey", rating: "3.5/5" }
+    {
+      name: "Mouna Ragam",
+      rating: "4/5",
+      description:
+        "Description:Divya, a woman grieving over the death of her lover, is convinced into an arranged marriage with Chandra Kumar. Over a year the couple attempt to adjust to each other's presence and live with each other."
+    },
+    {
+      name: "alai payuthey",
+      rating: "3.5/5",
+      description:
+        "Description:After a difficult romance in a class-divided society, the love birds find out marriage is not what they thought it would be."
+    },
+    {
+      name: "vaaranam aayiram",
+      rating: "3.8/5",
+      description:
+        "Description:A father and son help each other through growing up, romance, tragedy, and adventure."
+    }
   ],
   Biopic: [
-    { name: "M.S Dhoni", rating: "4.5/5" },
-    { name: "mahanati", rating: "4/5" }
+    {
+      name: "M.S Dhoni",
+      rating: "4.5/5",
+      description:
+        "Description:The untold story of Mahendra Singh Dhoni's journey from ticket collector to trophy collector - the world-cup-winning captain of the Indian Cricket Team."
+    },
+    {
+      name: "mahanati",
+      rating: "4/5",
+      description:
+        "Description:Biography of Savitri, an actress from South India movie industry, who ruled the industry for two decades during 50s and 60s."
+    },
+    {
+      name: "soorarai potru",
+      rating: "4.8/5",
+      description:
+        "Description:Nedumaaran Rajangam Maara sets out to make the common man fly and in the process takes on the world's most capital intensive industry and several enemies who stand in his way."
+    }
   ]
 };
 
@@ -77,6 +141,7 @@ export default function App() {
 
               <div style={{ fontsize: "larger" }}> {film.name}</div>
               <div style={{ fontsize: "smaller" }}> {film.rating}</div>
+              <div style={{ fontsize: "smaller" }}> {film.description}</div>
             </li>
           ))}
         </ul>
